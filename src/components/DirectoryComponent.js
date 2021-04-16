cb2713112f0d0ecbd7773b3a80adab2e9f4342b5
Davtand27 <79177820+Davtand27@users.noreply.github.com>
2021-04-15 20:19:30 -0700

    functional components (refactoring)

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
-import CampsiteInfo from "./CampsiteInfoComponent"
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component { 
+function RenderDirectoryItem({campsite, onClick}) {
+    return(
+        <Card onClick={() => onClick(campsite.id)}>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+            <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+    </Card>
+    );
+}
 
+function Directory(props) {
     
-
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+    const directory = props.campsites.map(campsite => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-
-                
-
-                {/* <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div> */}
-               {/* <ExampleParentComponent /> */}
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
+
 
 // class ExampleParentComponent extends Component {
 //     constructor(props) {
