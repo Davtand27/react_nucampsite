0048d1b65d671588702596483c45bd93362f84f8
Davtand27 <79177820+Davtand27@users.noreply.github.com>
2021-04-24 00:22:02 -0700

    React Redux Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,6 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
