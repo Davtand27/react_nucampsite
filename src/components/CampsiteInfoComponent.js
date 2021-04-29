21471b779bd077b3193784b52196af775bf833bf
Davtand27 <79177820+Davtand27@users.noreply.github.com>
2021-04-28 19:03:34 -0700

    redux thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,9 +3,9 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,
     Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = len => val => !(val) || (val.length <= len);
-
 const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}) {
@@ -121,31 +121,31 @@ class CommentForm extends Component {
         );
     }
 }
+
 function CampsiteInfo(props) {
-    if (props.campsite) {
+    if (props.isLoading) {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
-                        comments={props.comments}
-                        addComment={props.addComment}
-                        campsiteId={props.campsite.id}
-                    />
+                    <Loading />
                 </div>
             </div>
         );
     }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    if (props.campsite) {
     return <div />;
+    }
 }
+
 export default CampsiteInfo;
